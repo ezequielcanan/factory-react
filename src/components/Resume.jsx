9d671cd517a9d182f560ec96b34251985cdbc422
Ezequiel Canan <ezequielcanan@gmail.com>
2024-10-23 22:05:00 -0300

    Home

---
+++ src/components/Resume.jsx
@@ -0,0 +1,177 @@
+import { motion } from "framer-motion"
+import { useEffect, useState } from "react"
+import { Oval } from "react-loader-spinner"
+import customAxios from "../config/axios.config"
+import moment from "moment"
+import { Line } from "react-chartjs-2"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import Input from "./Input"
+import Button from "./Button"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement, 
+  LineElement,  
+  Title,     
+  Tooltip,
+  Legend 
+);
+
+const Resume = ({ title, week = true, month = false, controls = false }) => {
+  const [resume, setResume] = useState(null)
+  const [error, setError] = useState(false)
+  const [labels, setLabels] = useState([])
+  const [profitsData, setProfitsData] = useState([])
+  const [from, setFrom] = useState(moment().subtract(month ? 31 : 7, "days"))
+  const [to, setTo] = useState(moment())
+  const [search, setSearch] = useState(false)
+
+  useEffect(() => {
+    customAxios.get(`/orders/recent${controls ? `?from=${from.format("DD-MM-YYYY")}&to=${to.format("DD-MM-YYYY")}` : ((month) ? `?from=${moment().subtract(31, "days").format("DD-MM-YYYY")}` : "")}`).then(res => {
+      setResume(res?.data)
+    }).catch(e => {
+      setError(true)
+    })
+  }, [search])
+
+  useEffect(() => {
+    const generateLabels = () => {
+      let days = [];
+      const differenceBetweenActualDate = moment().diff(to, "days")
+      const totalDays = (controls ? (to.diff(from, "days")) : (week && !month) ? 7 : 30)
+      for (let i = totalDays; i >= 0; i--) {
+        days.push(moment().subtract(i+differenceBetweenActualDate, "days").format("DD-MM"));
+      }
+      setLabels(days);
+      return days
+    };
+
+    
+    const calculateProfits = (days = labels) => {
+      if (!resume) return [];
+      
+      let dailyProfits = [];
+      let accumulatedProfit = 0;
+      
+      const sortedOrders = resume?.orders?.sort((a, b) =>
+        moment(a?.finalDate, "DD-MM-YYYY")?.diff(moment(b?.finalDate, "DD-MM-YYYY"))
+    );
+    
+      const profitsByDate = {};
+
+      sortedOrders.forEach((order) => {
+        const date = moment(order?.finalDate).format("DD-MM");
+
+        const orderProfit = order?.articles?.reduce(
+          (artAcc, art) =>
+            artAcc + (art?.price || 0) * (art?.quantity || 0) * (order?.mode ? 1.21 : 1),
+          0
+        );
+
+        if (profitsByDate[date]) {
+          profitsByDate[date] += orderProfit;
+        } else {
+          profitsByDate[date] = orderProfit;
+        }
+      });
+
+      days.forEach((day) => {
+        if (profitsByDate[day]) {
+          accumulatedProfit += profitsByDate[day];
+        }
+        dailyProfits.push(accumulatedProfit);
+      });
+
+      setProfitsData(dailyProfits);
+    };
+
+    const days = generateLabels();
+    calculateProfits(days);
+  }, [resume]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '',
+        data: profitsData,
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }
+    ]
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          font: {
+            size: 8,
+          }
+        }
+      },
+      y: {
+        ticks: {
+          font: {
+            size: 8,
+          }
+        }
+      }
+    }
+  };
+
+  const onChangeDate = (setFunc, value, subtract = false) => {
+    setFunc(!subtract ? moment(value) : moment(value))
+  }
+
+  const textClassName = "text-md text-white"
+
+  return resume ? (
+    <>
+      <motion.section initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} transition={{ duration: 0.5 }} className="bg-third rounded-xl row-span-2 shadow-[0_0px_10px_1px_rgba(0,255,255,0.2)] p-4 flex flex-col gap-y-8 text-white">
+        {!controls ? <h2 className="text-xl text-white font-bold mb-2">Ultimo mes</h2> : (
+          <div className="flex flex-wrap gap-4 items-center">
+            <div className="flex items-center gap-4">
+              <p className="text-lg font-bold">Desde:</p>
+              <Input type="date" className="text-sm !p-2" defaultValue={from.format("YYYY-MM-DD")} onInput={(e) => onChangeDate(setFrom,e?.target?.value)}/>
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="text-lg font-bold">Hasta:</p>
+              <Input type="date" className="text-sm !p-2" defaultValue={to.format("YYYY-MM-DD")} onInput={(e) => onChangeDate(setTo,e?.target?.value, true)}/>
+            </div>
+            <Button className={"text-sm"} onClick={() => setSearch(s => !s)}>Buscar</Button>
+          </div>
+        )}
+        <p className={textClassName}>Ganancia: ${resume?.profits}</p>
+        <p className={textClassName}>Pedidos facturados: {resume?.ordersLength}</p>
+        <p className={textClassName}>Articulos diferentes: {resume?.articles?.length}</p>
+        <Line key={JSON.stringify(resume?.articles)} data={data} options={options} className="text-sm mt-auto"/>
+      </motion.section>
+    </>
+  ) : (
+    !error ? (
+      <Oval />
+    ) : (
+      <p className="text-2xl text-red-600">ERROR</p>
+    )
+  )
+}
+
+export default Resume
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
+import { useEffect, useState } from "react"
 import Main from "../containers/Main"
 import { motion } from "framer-motion"
+import customAxios from "../config/axios.config"
+import { Oval } from "react-loader-spinner"
+import Resume from "../components/Resume"
 
 const Home = () => {
   return (
-    <Main className={"grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 gap-8"}>
-      <motion.section initial={{height: 0, opacity: 0}} animate={{height: "auto", opacity: 1}} transition={{duration: 0.5}} className="bg-third rounded-lg border-2 border-cyan-500 row-span-2 shadow-[0_0px_10px_1px_rgba(0,255,255,0.8)]">
-        <h2 className="text-3xl text-white font-bold p-4">Ultima semana</h2>
-      </motion.section>
-      <motion.section initial={{height: 0, opacity: 0}} animate={{height: "auto", opacity: 1}} transition={{duration: 0.5}} className="bg-third rounded-lg border-2 border-cyan-500 row-span-2 shadow-[0_0px_10px_1px_rgba(0,255,255,0.8)]">
-        <h2 className="text-3xl text-white font-bold p-4">Ultimo mes</h2>
-      </motion.section>
+    <Main className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 gap-8"}>
+      <Resume/>
+      <Resume month/>
+      <Resume controls/>
     </Main>
   )
 }
